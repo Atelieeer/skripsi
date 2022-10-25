aff15785e3669b69aca7c934ec1e5b9463ce9414
Atelieeer <reparationatelier123@gmail.com>
2022-10-25 12:10:52 +0800

    google map

--- app/src/main/java/id/kharisma/studio/atelier/DetailPemesanan.java
+++ app/src/main/java/id/kharisma/studio/atelier/DetailPemesanan.java
@@ -2,13 +2,26 @@ package id.kharisma.studio.atelier;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DetailPemesanan extends AppCompatActivity {
-
+    Intent intent;
+    Button btnPilihLokasi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_pemesanan);
+        btnPilihLokasi = findViewById(R.id.btnPilihLokasi);
+        btnPilihLokasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(DetailPemesanan.this, GoogleMap.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
